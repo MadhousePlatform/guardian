e0a3b09fcc44a526c209b255f30c775e7e3c6c01
Sketch <sketch@sketchni.uk>
2023-12-31 03:14:29 +0000

    Update
    Signed-off-by: Sketch <sketch@sketchni.uk>

--- src/servers.rs
+++ src/servers.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
 use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE, ACCEPT, USER_AGENT};
 use reqwest::{Client, Error};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use tokio::time::Instant;
+
+use crate::{tolerance};
 
 #[derive(Deserialize, Debug)]
 pub struct ServerInfo {
@@ -28,7 +32,7 @@ pub struct CurrentState {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Attributes {
-    pub attributes: CurrentState
+    pub attributes: CurrentState,
 }
 
 /// Get a list of all servers.
@@ -98,4 +102,84 @@ fn set_headers(key: String) -> HeaderMap {
     headers.insert(ACCEPT, "application/json".parse().unwrap());
     headers.insert(USER_AGENT, "guardian/0.1.0".parse().unwrap());
     return headers;
+}
+
+/// Get a list of all servers.
+pub async fn map_servers() -> HashMap<String, HashMap<String, String>> {
+    let servers: Result<Vec<ServerInfo>, Error> = get_servers().await;
+    match servers {
+        Ok(servers) => {
+            /* This is cheap, nasty and vulgar but we're using a
+             * HashMap inside a HashMap to emulate a PHP array
+             * because I don't know how else to store this data.
+             * - Sketch, 28/12/23
+             */
+            let mut server_map = HashMap::new();
+
+            /* Index the HashMap inside the HashMap from 0 */
+            let mut i = 0;
+            for server in servers {
+                let p = ping_server(&server).await;
+                // We don't want to check stopped servers.
+                if p != "stopped" {
+                    /* Build the inner HashMap */
+                    let mut inner = HashMap::new();
+                    inner.insert("server".to_string(), server.identifier.to_string());
+                    inner.insert("name".to_string(), server.name.to_string());
+                    inner.insert("slow".to_string(), 4.to_string());
+                    /* Insert inner HashMap to outer HashMap */
+                    server_map.insert(i.to_string(), inner);
+                }
+                /* Increment the index */
+                i += 1;
+            }
+
+            println!("{:?}", server_map);
+            return server_map;
+        }
+        Err(e) => {
+            panic!("An error occurred: {:?}", e)
+        }
+    }
+}
+
+/// Ping the specified server and check if the response
+/// time is within tolerance.
+///
+/// # Arguments
+///
+/// * `server` - `&ServerInfo` - The identifier of the server you want to check.
+///
+/// # Returns
+///
+/// `&str` - "fast", "slow", "stopped".
+pub async fn ping_server(server: &ServerInfo) -> &str {
+    let id: &String = &server.identifier;
+    let response: Result<String, Error> = get_server_state(id).await;
+
+    if response.unwrap().trim() == "running" {
+        let duration: Instant = Instant::now();
+
+        send_list_command(id).await;
+        let elapsed: u128 = duration.elapsed().as_millis();
+        let tol: &str = tolerance::within_tolerance(elapsed);
+        println!("server: {:?}, time: {:?}ms, within tol (1200ms): {:?}", &server.name, elapsed, &tol);
+        return tol;
+    }
+
+    return "stopped";
+}
+
+/*async fn send_tps_request(server: String) {
+    servers::send_command(server.parse().unwrap(), "forge tps").await.expect("()");
+}
+*/
+
+/// Send the LIST command to the minecraft server.
+///
+/// # Arguments
+///
+/// * `identifier` - `&String` - The identifier of the server you want to check.
+pub async fn send_list_command(identifier: &String) {
+    send_command(identifier, "list").await.expect("()");
 }
